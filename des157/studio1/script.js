567adc6ae6fadba0e26a42cbb1172a52457bbd25
Caroline Yau <carolineqingyuyau@gmail.com>
2021-02-03 01:54:25 +0800

    form javascript bug fixed, completed

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -4,31 +4,40 @@
 
     const btn = document.getElementById('submit');
     const rtn = document.getElementById('return');
-    var product = document.getElementById('product').value.toLowerCase();
-    var customer = document.getElementById('customer').value.toLowerCase();
-    const goal = document.getElementById('goal').value.toLowerCase();
-    const verb1 = document.getElementById('verb1').value.toLowerCase();
-    const gain = document.getElementById('gain').value.toLowerCase();
-    const verb2 = document.getElementById('verb2').value.toLowerCase();
-    const pain = document.getElementById('pain').value.toLowerCase();
-    const competitor = document.getElementById('competitor').value.toLowerCase();
     var fill = document.getElementById('fill');
     var result = document.getElementById('result');
     var output = document.getElementById('output');
 
     btn.onclick = function(){
-        var bproduct = document.getElementById('boldProduct').innerHTML = '<b>product</b>';
-        var bcustomer = document.getElementById('boldCustomer').innerHTML = '<b>customer</b>';
+        var product = document.getElementById('product').value.toLowerCase();
+        var customer = document.getElementById('customer').value.toLowerCase();
+        const goal = document.getElementById('goal').value.toLowerCase();
+        const verb1 = document.getElementById('verb1').value.toLowerCase();
+        const gain = document.getElementById('gain').value.toLowerCase();
+        const verb2 = document.getElementById('verb2').value.toLowerCase();
+        const pain = document.getElementById('pain').value.toLowerCase();
+        const competitor = document.getElementById('competitor').value.toLowerCase();
+
+        document.getElementById('boldProduct').innerHTML = product;
+        document.getElementById('boldCustomers').innerHTML = customer;
+        document.getElementById('boldGoal').innerHTML = goal;
+        document.getElementById('boldVerb1').innerHTML = verb1;
+        document.getElementById('boldGain').innerHTML = gain;
+        document.getElementById('boldVerb2').innerHTML = verb2;
+        document.getElementById('boldPain').innerHTML = pain;
+        document.getElementById('boldCompetitor').innerHTML = competitor;
 
         result.style.display = "block";
-        // output.innerHTML = product;
-        // output.innerHTML = "Our " + product + " helps " + customer + " who wants to " + goal + " by " + verb1 + " " + gain + " and " + verb2 + " " + pain + " unlike " + competitor;
-                // myArticle.innerHTML = '<p>I like cheese!<p>';
         fill.style.display = "none";
         btn.style.display = "none";
         rtn.style.display = "block";
-        
     }
 
+    rtn.onclick = function(){
+        result.style.display = "none";
+        fill.style.display = "block";
+        btn.style.display = "block";
+        rtn.style.display = "none";
+    }
 
 })();
