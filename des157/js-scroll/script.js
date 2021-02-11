d2ba106ab98d5e847a6a3a92e9eae9a5e9479c81
Caroline Yau <carolineqingyuyau@gmail.com>
2021-02-12 00:57:03 +0800

    added new articles to learning journal and added javascript scroll webpage

---
+++ des157/js-scroll/script.js
@@ -0,0 +1,87 @@
+(function () {
+    'use strict';
+
+    // start your script here...
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(function(eachLink){
+        eachLink.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(event){
+        event.preventDefault();
+
+        const targetID = event.target.getAttribute('href');
+        const targetAnchor = document.querySelector(targetID);
+        // console.log(targetAnchor.getBoundingClientRect().top);
+        const originalTop = Math.floor(targetAnchor.getBoundingClientRect().top) - 200;
+        window.scrollBy({top: originalTop, left: 0, behavior: 'smooth'});
+        console.log(originalTop);
+
+        window.addEventListener('load', function(){
+            const posts = document.querySelectorAll('section');
+            let postTops = [];
+            let pageTop;
+            let counter = 1;
+            let prevCounter = 1;
+            let doneResizing;
+
+            console.log(posts[0].getBoundingClientRect().top) + window.pageYOffset;
+        
+            /* posts.forEach(function(post){
+                postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
+            });
+    
+            console.log(postTops); */
+
+            resetPagePosition();
+
+            window.addEventListener('scroll', function(){
+                pagetop = window.pageYOffset + 250;
+                // console.log(pagetop);
+                if (pagetop > pageTops[counter]){
+                    counter++;
+                    console.log(`scrolling down ${counter}`);
+                } else if (counter > 1 && pagetop < postTops[counter-1]){
+                    counter--;
+                    console.log(`scrolling up ${counter}`);
+                }
+                if (counter != prevCounter){
+                    navLinks.forEach(function(eachLink){
+                        eachLink.removeAttribute('class');
+                    });
+
+                    const thisLink = document.querySelector(`nav ul li:nth-child(${counter}) a`);
+
+                    thisLink.className = 'selected';
+                    prevCounter = counter;
+                }
+            });
+
+            window.addEventListener('resize', function(){
+                clearTimeout(doneResizing);
+                doneResizing = setTimeout(function(){
+                    resetPagePosition();
+                }, 500);
+            });
+        });
+
+        function resetPagePosition(){
+            postTops = [];
+            posts.forEach(function(post){
+                postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
+            });
+
+            const pagePosition = window.pageYOffset + 250;
+            counter = 0;
+
+            postTops.forEach(function(post){if(pagePosition > post){counter++;}});
+            navLinks.forEach(function(eachLink){eachLink.removeAttribute('class');});
+
+            const thisLink = document.querySelector(`nav ul li:nth-child(${counter}) a`);
+            thisLink.className = 'selected';
+        }
+
+    }
+
+})();
