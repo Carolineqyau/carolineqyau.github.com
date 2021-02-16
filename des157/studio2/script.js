491507622efc0bdd7085b5cddd94fa7e1d39196a
Caroline Yau <carolineqingyuyau@gmail.com>
2021-02-17 04:47:24 +0800

    javascript transition effects on image zoom upon click event

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -3,19 +3,24 @@
     console.log("reading js");
     // const page = document.querySelector('body');
     const els = document.getElementsByClassName('grid-gallery'); //get grid gallery element
+    const lay = document.getElementById('overlay');
+    let sel = false;
     for (let i=0; i<els.length; i++){ //iterate through grid gallery to identify all images
         els[i].addEventListener('click', function(event) { //click event on any of the images
             event.preventDefault();
             let selection = event.target; //the target of click event is the selected image
             let originalWidth = selection.clientWidth; //get the width of loaded image on website
-            selection.classList.add("select"); //add select class to format the clicked image 
-            //let deselection = document.querySelector('.select'); //identify the selected image to prepare for deselection
-            console.log(selection)
-            selection.addEventListener('click', function(){ //deselection click event
+            if (!sel){
+                // selection.style.marginTop = `-${selection.clientHeight/2}`;
+                // selection.style.marginLeft = `-${selection.clientWidth/2}`;
+                selection.classList.add("select"); //add select class to format the clicked image 
+                lay.style.visibility = "visible";
+            } else {
+                selection.classList.remove("select");
+                lay.style.visibility = "hidden";
+            }
+            sel = !sel
 
-                selection.classList.remove("select"); //remove select class formatting to return to original
-                console.log("Clicked Image")
-            });
         });
     }
 
