6418092c29c3e0f833d5b726579d614a318132dd
Caroline Yau <carolineqingyuyau@gmail.com>
2021-02-17 05:45:10 +0800

    changing background color added,

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,11 +1,19 @@
 (function(){
     "use strict";
     console.log("reading js");
-    // const page = document.querySelector('body');
+    const page = document.querySelector('body');
     const els = document.getElementsByClassName('grid-gallery'); //get grid gallery element
     const lay = document.getElementById('overlay');
     let sel = false;
     for (let i=0; i<els.length; i++){ //iterate through grid gallery to identify all images
+        els[i].addEventListener('mouseover', function(eachImg){
+            page.style.backgroundColor = "#F6F3EE";
+        });
+
+        els[i].addEventListener('mouseout', function(eachImg){
+            page.style.backgroundColor = "#A19C95";
+        });
+        
         els[i].addEventListener('click', function(event) { //click event on any of the images
             event.preventDefault();
             let selection = event.target; //the target of click event is the selected image
