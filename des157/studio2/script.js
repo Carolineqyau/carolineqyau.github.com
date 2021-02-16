66936735cf02648771e25b6198bc803768cfd569
Caroline Yau <carolineqingyuyau@gmail.com>
2021-02-17 03:38:26 +0800

    edited script

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,7 +1,7 @@
 (function(){
     "use strict";
     console.log("reading js");
-    const page = document.querySelector('body');
+    // const page = document.querySelector('body');
     const els = document.getElementsByClassName('grid-gallery'); //get grid gallery element
     for (let i=0; i<els.length; i++){ //iterate through grid gallery to identify all images
         els[i].addEventListener('click', function(event) { //click event on any of the images
@@ -9,12 +9,14 @@
             let selection = event.target; //the target of click event is the selected image
             let originalWidth = selection.clientWidth; //get the width of loaded image on website
             selection.classList.add("select"); //add select class to format the clicked image 
+            //let deselection = document.querySelector('.select'); //identify the selected image to prepare for deselection
+            console.log(selection)
+            selection.addEventListener('click', function(){ //deselection click event
+
+                selection.classList.remove("select"); //remove select class formatting to return to original
+                console.log("Clicked Image")
+            });
         });
     }
-
-    // let deselection = document.querySelector('.select'); //identify the selected image to prepare for deselection
-    // deselection.addEventListener('click', function(){ //deselection click event
-    //     deselection.classList.remove("select"); //remove select class formatting to return to original
-    // });
 
 })();
