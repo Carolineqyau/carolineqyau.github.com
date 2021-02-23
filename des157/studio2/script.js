1b8686acce7795d667e873db7d270538c713c32b
Caroline Yau <carolineqingyuyau@gmail.com>
2021-02-24 02:03:16 +0800

    studio 2 edited

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,20 +1,36 @@
+
 (function(){
     "use strict";
     console.log("reading js");
     const page = document.querySelector('body');
     const els = document.getElementsByClassName('grid-gallery'); //get grid gallery element
+    const lay = document.getElementById('overlay');
+    let sel = false;
     for (let i=0; i<els.length; i++){ //iterate through grid gallery to identify all images
+        els[i].addEventListener('mouseover', function(eachImg){
+            page.style.backgroundColor = "#F6F3EE";
+        });
+
+        els[i].addEventListener('mouseout', function(eachImg){
+            page.style.backgroundColor = "#A19C95";
+        });
+        
         els[i].addEventListener('click', function(event) { //click event on any of the images
             event.preventDefault();
             let selection = event.target; //the target of click event is the selected image
             let originalWidth = selection.clientWidth; //get the width of loaded image on website
-            selection.classList.add("select"); //add select class to format the clicked image 
+            if (!sel){
+                // selection.style.marginTop = `-${selection.clientHeight/2}`;
+                // selection.style.marginLeft = `-${selection.clientWidth/2}`;
+                selection.classList.add("select"); //add select class to format the clicked image 
+                lay.style.visibility = "visible";
+            } else {
+                selection.classList.remove("select");
+                lay.style.visibility = "hidden";
+            }
+            sel = !sel
+
         });
     }
-
-    // let deselection = document.querySelector('.select'); //identify the selected image to prepare for deselection
-    // deselection.addEventListener('click', function(){ //deselection click event
-    //     deselection.classList.remove("select"); //remove select class formatting to return to original
-    // });
 
 })();
