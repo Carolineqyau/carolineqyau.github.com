4603e009ed986fde4f17031cd0d976305287db65
Carolineqyau <carolineqingyuyau@gmail.com>
2022-02-15 07:58:11 -0800

    narrative completed

--- des157b/studio3/script.js
+++ des157b/studio3/script.js
@@ -1,39 +1,132 @@
-document.querySelector('#granim-canvas')
+
+// Also can pass in optional settings block
+var rellax = new Rellax('.rellax', {
+    speed: -6,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
+
+
+// document.querySelector('#granim-canvas')
 
 // var granimInstance = new Granim({
-//     element: '#granim-canvas',
-//     name: 'granim',
-//     opacity: [1, 1],
+//    element: '#granim-canvas',
+//    name: 'granim',
+//    opacity: [1, 1],
+//    states : {
+//        "default-state": {
+//            gradients: [
+//                ['#834D9B', '#D04ED6'],
+//                ['#1CD8D2', '#93EDC7']
+//            ]
+//        }
+//    }
+// });
+
+// var granimInstance = new Granim({
+//     element: '#canvas-basic',
+//     direction: 'left-right',
+//     isPausedWhenNotInView: true,
 //     states : {
 //         "default-state": {
 //             gradients: [
-//                 ['#834D9B', '#D04ED6'],
-//                 ['#1CD8D2', '#93EDC7']
+//                 ['#ff9966', '#ff5e62'],
+//                 ['#00F260', '#0575E6'],
+//                 ['#e1eec3', '#f05053']
 //             ]
 //         }
 //     }
-//  });
+// });
 
-var granimInstance = new Granim({
-    element: '#canvas-basic',
-    direction: 'left-right',
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#ff9966', '#ff5e62'],
-                ['#00F260', '#0575E6'],
-                ['#e1eec3', '#f05053']
-            ]
-        }
-    }
-});
+// // new fullpage('#fullpage', {
+// // 	//options here
+// // 	autoScrolling:true,
+// // 	scrollHorizontally: true
+// // });
 
-new fullpage('#fullpage', {
-	//options here
-	autoScrolling:true,
-	scrollHorizontally: true
-});
+// // //methods
+// // fullpage_api.setAllowScrolling(false);
 
-//methods
-fullpage_api.setAllowScrolling(false);
+// var myFullpage = new fullpage('#fullpage', {
+//     //Navigation
+//     menu: '#menu',
+//     lockAnchors: false,
+//     anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+//     navigation: false,
+//     navigationPosition: 'right',
+//     navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide', 'fourthSlide'],
+//     showActiveTooltip: false,
+//     slidesNavigation: false,
+//     slidesNavPosition: 'bottom',
+
+//     //Scrolling
+//     css3: true,
+//     scrollingSpeed: 700,
+//     autoScrolling: true,
+//     fitToSection: true,
+//     fitToSectionDelay: 1000,
+//     scrollBar: false,
+//     easing: 'easeInOutCubic',
+//     easingcss3: 'ease',
+//     loopBottom: false,
+//     loopTop: false,
+//     loopHorizontal: true,
+//     continuousVertical: false,
+//     continuousHorizontal: false,
+//     scrollHorizontally: false,
+//     interlockedSlides: false,
+//     dragAndMove: false,
+//     offsetSections: false,
+//     resetSliders: false,
+//     fadingEffect: false,
+//     normalScrollElements: '#element1, .element2',
+//     scrollOverflow: false,
+//     scrollOverflowReset: false,
+//     scrollOverflowOptions: null,
+//     touchSensitivity: 15,
+//     bigSectionsDestination: null,
+
+//     //Accessibility
+//     keyboardScrolling: true,
+//     animateAnchor: true,
+//     recordHistory: true,
+
+//     //Design
+//     controlArrows: true,
+//     verticalCentered: true,
+//     sectionsColor : ['black','#FF97A4', '#358DA4', '#FD4D65', '#387C81'],
+//     paddingTop: '3em',
+//     paddingBottom: '10px',
+//     fixedElements: '#header, .footer',
+//     responsiveWidth: 0,
+//     responsiveHeight: 0,
+//     responsiveSlides: false,
+//     parallax: true,
+//     parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+//     dropEffect: false,
+//     dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
+//     waterEffect: false,
+//     waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
+//     cards: false,
+//     cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+
+//     //Custom selectors
+//     sectionSelector: '.section',
+//     slideSelector: '.slide',
+
+//     lazyLoading: true,
+
+//     //events
+//     onLeave: function(origin, destination, direction){},
+//     afterLoad: function(origin, destination, direction){},
+//     afterRender: function(){},
+//     afterResize: function(width, height){},
+//     afterReBuild: function(){},
+//     afterResponsive: function(isResponsive){},
+//     afterSlideLoad: function(section, origin, destination, direction){},
+//     onSlideLeave: function(section, origin, destination, direction){}
+// });
+
